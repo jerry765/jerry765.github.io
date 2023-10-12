587a918da24c8fc57fc03d257a29c643e5ffce28
moke <2061610579@qq.com>
2023-10-12 20:01:34 +0800

    Site updated: 2023-10-12 20:01:33

--- code/c/list.c
+++ code/c/list.c
@@ -82,4 +82,30 @@ Position FindPrevious(ElementType X, List L) {
     return P;
 }
 
-// 
+// Insert (after legal position P)
+// Header implementation assumed
+// Parameter L is unused in this implementation
+void Insert(ElementType X, List L, Position P) {
+    Position TmpCell;
+
+    TmpCell = malloc(sizeof(struct Node));
+    if(TmpCell == NULL) {
+        FatalError("Out of space!!!");
+    }
+
+    TmpCell->Element = X;
+    TmpCell->Next = P->Next;
+    P->Next = TmpCell;
+}
+
+void DeleteList(List L) {
+    Position P, Tmp;
+
+    P = L->Next;    // Header assumed
+    L->Next = NULL;
+    while (P != NULL) {
+        Tmp = P->Next;
+        free(P);
+        P = Tmp;
+    }
+}
